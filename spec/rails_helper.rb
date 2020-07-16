dd957e03723dc7f8efd84f3d981c7b86d08224e2
jdmartinez1062 <jd.martinez1062@gmail.com>
2020-07-16 13:10:08 -0500

    fized file storage when running test suite

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,9 +69,3 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 end
-CarrierWave.configure do |config|
-  config.root = Rails.root.join('spec/fixtures')
-  config.cache_only = true
-  config.enable_processing = false
-  config.base_path = '/'
-end
