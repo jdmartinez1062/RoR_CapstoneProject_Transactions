78c7164f79d569a1ef559b1d4045d7d52bc86400
jdmartinez1062 <jd.martinez1062@gmail.com>
2020-07-29 17:47:21 -0500

    fixed rubocop issues

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,8 @@
 class Group < ApplicationRecord
   belongs_to :selected, class_name: :User, inverse_of: :groups
 
-  has_and_belongs_to_many :time_spents, association_foreign_key: :time_log_id, class_name: :TimeSpent, inverse_of: :groups, join_table: 'groups_times'
+  has_and_belongs_to_many :time_spents, association_foreign_key: :time_log_id, class_name: :TimeSpent,
+                                        inverse_of: :groups, join_table: 'groups_times'
   mount_uploader :icon, IconUploader
   validates :name, presence: true, length: { maximum: 20 }
 
--- spec/integration/transactions_spec.rb
+++ spec/integration/transactions_spec.rb
@@ -1,8 +1,7 @@
-def log_in_with(name)
-  visit login_path
+require './spec/helpers/helpers'
 
-  fill_in 'Name', with: name
-  click_button 'Log in'
+RSpec.configure do |c|
+  c.include Helpers
 end
 
 RSpec.feature 'Transactions', type: :feature do
@@ -14,8 +13,7 @@ RSpec.feature 'Transactions', type: :feature do
   let!(:transaction1) { group1.time_spents.create(name: 'Movement script', amount: 2, author_id: user1.id) }
 
   scenario 'User creates a new transaction with group' do
-    visit login_path
-    log_in_with(user1.name)
+    standard_procedure
     visit new_time_spent_path
 
     initial_count = TimeSpent.count
@@ -32,8 +30,7 @@ RSpec.feature 'Transactions', type: :feature do
   end
 
   scenario 'User creates a new transaction without group' do
-    visit login_path
-    log_in_with(user1.name)
+    standard_procedure
     visit new_time_spent_path
     initial_count = TimeSpent.count
 
@@ -46,46 +43,5 @@ RSpec.feature 'Transactions', type: :feature do
     final_count = TimeSpent.count
 
     expect(final_count - initial_count).to eq(1)
-  end
-
-  scenario 'User visits my group transactions page' do
-    visit login_path
-    log_in_with(user1.name)
-    visit user_path(user1)
-    click_link('My work times')
-
-    transactions_i = user1.group_time_spents.paginate(page: 1)
-    transactions_i.each do |transaction|
-      expect(page).to have_css('p', text: transaction.time_name.to_s)
-      expect(page).to have_css('p', text: transaction.time_amount.to_s)
-      expect(page).to have_css('p', text: transaction.time_log.to_s)
-      expect(page).to have_css("img[src*= '#{transaction.icon.url}']")
-    end
-  end
-  scenario 'User visits external transactions page' do
-    visit login_path
-    log_in_with(user1.name)
-    visit user_path(user1)
-    click_link('My external times')
-
-    transactions_i = user1.external_time_spents.paginate(page: 1)
-    transactions_i.each do |transaction|
-      expect(page).to have_css('p', text: transaction.time_name.to_s)
-      expect(page).to have_css('p', text: transaction.time_amount.to_s)
-      expect(page).to have_css('p', text: transaction.time_log.to_s)
-    end
-  end
-  scenario 'User visits the group transactions page' do
-    visit login_path
-    log_in_with(user1.name)
-    visit group_times_path(group1)
-
-    transactions_i = group1.group_spents.paginate(page: 1)
-    transactions_i.each do |transaction|
-      expect(page).to have_css('p', text: user1.name)
-      expect(page).to have_css('p', text: transaction.time_name.to_s)
-      expect(page).to have_css('p', text: transaction.time_amount.to_s)
-      expect(page).to have_css('p', text: transaction.time_log.to_s)
-    end
   end
 end
