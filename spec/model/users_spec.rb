12254937e8af8e65401c5604d1f101ca41dfe153
jdmartinez1062 <jd.martinez1062@gmail.com>
2020-09-17 09:54:50 -0500

    added association test for time spent with user and group and external time spent with user

--- spec/model/users_spec.rb
+++ spec/model/users_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe User, type: :model do
       expect(user1.name).to eql(group1.selected.name)
     end
   end
+
+  describe 'time spent relation' do
+    it 'builds a time relation with a group' do
+      user1.groups.create(name: 'dev1', icon: test_img_path)
+      group.time_spents.create(name: 'C#', amount: 3, author_id: user1.id)
+      expect(user1).to eql(TimeSpent.find_by(author_id: user1.id).author)
+    end
+
+    it 'builds a time relation with out a group' do
+      user1.time_spents.create(name: 'C#', amount: 3)
+      expect(user1).to eql(TimeSpent.find_by(author_id: user1.id).author)
+    end
+  end
 end
